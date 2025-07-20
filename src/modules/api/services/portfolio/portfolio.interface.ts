f371f0b91825672871b5ddcd9a1707b7fa53bdfd
AmirKhodam <77395761+amirkhodam@users.noreply.github.com>
2025-07-20 18:09:40 +0330

    refactor(portfolio): update title type in IPortfolio interface and enhance PortfolioService with getPortfolio method

--- src/modules/api/services/portfolio/portfolio.interface.ts
+++ src/modules/api/services/portfolio/portfolio.interface.ts
@@ -1,8 +1,8 @@
-import type { ITranslatedStrings } from '@/modules/api'
+import type { ITranslatedString, ITranslatedStrings } from '@/modules/i18n'
 
 export interface IPortfolio {
   id: string
-  title: ITranslatedStrings
+  title: ITranslatedString
   videos: string[]
   images: string[]
   texts: ITranslatedStrings
--- src/modules/api/services/portfolio/portfolio.service.ts
+++ src/modules/api/services/portfolio/portfolio.service.ts
@@ -3,6 +3,10 @@ import type { IPortfolio } from './portfolio.interface'
 
 export class PortfolioService extends Service {
   async getPortfolios() {
-    return this.get<IPortfolio>('/portfolios')
+    return this.get<Array<IPortfolio>>('/portfolios')
+  }
+
+  async getPortfolio(id: string) {
+    return this.get<IPortfolio>(`/portfolios/${id}`)
   }
 }
