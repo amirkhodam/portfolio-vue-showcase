1b94ba943a59430c4d1dc7de198e13733740b648
AmirKhodam <77395761+amirkhodam@users.noreply.github.com>
2025-07-20 16:29:45 +0330

    refactor(i18n): remove unused i18n index file for validation

--- src/utils/validation/i18n/index.ts
+++
@@ -1,9 +0,0 @@
-// @ts-ignore
-import { createI18n } from 'vue-i18n'
-import ENValidationLangFile from '../../../assets/lang/en/validation'
-export const i18n = createI18n({
-  locale: 'en',
-  messages: {
-    en: ENValidationLangFile
-  }
-})
