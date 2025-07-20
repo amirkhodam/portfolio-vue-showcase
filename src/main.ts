da9065d8ab4c30ba782fc96ee7935badde921e7b
AmirKhodam <77395761+amirkhodam@users.noreply.github.com>
2025-07-20 18:47:28 +0330

    feat(primevue): add PrimeVue and PrimeIcons for enhanced UI components

--- src/main.ts
+++ src/main.ts
@@ -6,11 +6,14 @@ import { i18n } from './modules/i18n'
 
 import App from './App.vue'
 import router from './router'
+import PrimeVue from 'primevue/config'
+import '@primeuix/themes/aura'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
 app.use(i18n)
+app.use(PrimeVue)
 
 app.mount('#app')
