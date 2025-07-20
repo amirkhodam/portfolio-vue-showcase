ae67f002c8b1179c1c41ae80c66cacd1844e2cd7
AmirKhodam <77395761+amirkhodam@users.noreply.github.com>
2025-07-20 11:52:20 +0330

    chore(eslint): add ESLint configuration for Vue and TypeScript with custom rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -20,6 +20,7 @@ export default defineConfigWithVueTs(
   globalIgnores(['**/dist/**', '**/dist-ssr/**', '**/coverage/**']),
 
   pluginVue.configs['flat/essential'],
+  pluginVue.configs['flat/strongly-recommended'],
   vueTsConfigs.recommended,
   skipFormatting,
 )
