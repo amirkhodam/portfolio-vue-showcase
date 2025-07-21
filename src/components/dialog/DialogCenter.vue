b5de9547f6814bbd43a7429dc1a4a26592c2ecd5
AmirKhodam <77395761+amirkhodam@users.noreply.github.com>
2025-07-21 16:07:17 +0330

    fix(dialog): update DialogCenter component positioning to center it correctly on the screen

--- src/components/dialog/DialogCenter.vue
+++ src/components/dialog/DialogCenter.vue
@@ -7,7 +7,7 @@ const emit = defineEmits<{
 </script>
 
 <template>
-  <Dialog v-bind="$props" v-on="$attrs" class="flex h-screen w-screen items-center justify-center">
+  <Dialog v-bind="$props" v-on="$attrs" class="absolute top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2">
     <template v-for="(_, name) in $slots" #[name]="slotProps">
       <slot :name="name" v-bind="slotProps" />
     </template>
