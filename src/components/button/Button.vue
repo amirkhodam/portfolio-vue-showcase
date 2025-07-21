ba16be067ffefb461fad99b93b63bded8cbdb030
AmirKhodam <77395761+amirkhodam@users.noreply.github.com>
2025-07-21 10:07:31 +0330

    feat(button): create Button component using PrimeVue for enhanced UI interactions

---
+++ src/components/button/Button.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import Button from 'primevue/button'
+</script>
+
+<template>
+  <Button v-bind="$props" v-on="$attrs">
+    <template v-for="(_, name) in $slots" #[name]="slotProps">
+      <slot :name="name" v-bind="slotProps" />
+    </template>
+  </Button>
+</template>
